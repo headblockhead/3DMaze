9cd68a2241b151770a57385daad511503b5a76ce
headblockhead <26520767+headblockhead@users.noreply.github.com>
2020-10-25 20:52:04 +0000

    Fineshed maze

---
+++ install.bat
@@ -0,0 +1,5 @@
+
+mkdir "%programdata%/Microsoft/Windows/Start Menu/Programs/3DMAZE"
+cp arcadesim.exe "%programdata%/Microsoft/Windows/Start Menu/Programs/3DMAZE/"
+mkdir "%appdata%/3dmaze"
+cp textures.png  "%appdata%/3dmaze/"
